f4649591ca505d7a515680211fefed8e2ef1fe10
KatyPreston <prestonkj@hotmail.com>
2018-04-25 14:53:05 +0100

    files set up

---
+++ db/console.rb
@@ -0,0 +1,18 @@
+require("pry")
+require_relative("../models/albums")
+require_relative("../models/artists")
+
+artist1 = Artist.new({
+  "name" => "Queen"
+  })
+artist1.save()
+
+album1 = Album.new({
+  "artist_id" => artist1.id,
+  "title" => "On Air",
+  "genre" => "Rock"
+  })
+album1.save()
+
+binding.pry
+nil
